fee1f9e4509602289dd6ff06f0702f8caf7dcfb4
mavolin <github@mavolin.co>
2023-11-01 14:14:36 +0100

    fix: open ports needed by KDEConnect/GSConnect

--- system/configuration.nix
+++ system/configuration.nix
@@ -57,6 +57,16 @@
       127.0.0.1 c.loc
     '';
     networkmanager.enable = true;
+    
+    firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        { from = 1714; to = 1764; }
+      ];
+      allowedUDPPortRanges = [
+        { from = 1714; to = 1764; }
+      ];
+    };
   };
 
   powerManagement.enable = true;
