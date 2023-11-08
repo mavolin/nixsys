a786cbbe5bc5bd8ce1526d306ef2309b5bf8684c
mavolin <github@mavolin.co>
2023-11-08 12:40:26 +0100

    inst/rm: switch from tuxedo control center to tuxedo-rs

--- system/services/tuxedo.nix
+++ system/services/tuxedo.nix
@@ -17,45 +17,74 @@
   };
 
   environment.etc."tailord/profiles/default.json".text = builtins.toJSON {
-    fans = [ "silent" ];
-    leds = [{
-      device_name = "platform:tuxedo_keyboard";
-      function = "kbd_backlight";
-      profile = "none";
-    }];
+    fans = ["silent"];
+    leds = [
+      {
+        device_name = "platform:tuxedo_keyboard";
+        function = "kbd_backlight";
+        profile = "none";
+      }
+    ];
     performance_profile = "entertainment";
   };
   environment.etc."tailord/profiles/keyboard_lighting.json".text = builtins.toJSON {
-    fans = [ "silent" ];
-    leds = [{
-      device_name = "platform:tuxedo_keyboard";
-      function = "kbd_backlight";
-      profile = "dim";
-    }];
-    performance_profile = [ "entertainment" ];
+    fans = ["silent"];
+    leds = [
+      {
+        device_name = "platform:tuxedo_keyboard";
+        function = "kbd_backlight";
+        profile = "dim";
+      }
+    ];
+    performance_profile = ["entertainment"];
   };
   environment.etc."tailord/profiles/power_saving.json".text = builtins.toJSON {
-    fans = [ "silent" ];
-    leds = [{
-      device_name = "platform:tuxedo_keyboard";
-      function = "kbd_backlight";
-      profile = "none";
-    }];
-    performance_profile = [ "power_saving" ];
+    fans = ["silent"];
+    leds = [
+      {
+        device_name = "platform:tuxedo_keyboard";
+        function = "kbd_backlight";
+        profile = "none";
+      }
+    ];
+    performance_profile = ["power_saving"];
   };
-#
+  #
   environment.etc."tailord/keyboard/none.json".text = builtins.toJSON {
-    Single = {r = 0; g = 0; b = 0;};
+    Single = {
+      r = 0;
+      g = 0;
+      b = 0;
+    };
   };
   environment.etc."tailord/keyboard/dim.json".text = builtins.toJSON {
-    Single = {r= 14; g = 14; b = 14; };
+    Single = {
+      r = 14;
+      g = 14;
+      b = 14;
+    };
   };
 
   environment.etc."tailord/fan/silent.json".text = builtins.toJSON [
-    {temp = 60; fan = 0;}
-    {temp = 70; fan = 30;}
-    {temp = 75; fan = 50;}
-    {temp = 80; fan = 85;}
-    {temp = 90; fan = 100;}
+    {
+      temp = 50;
+      fan = 0;
+    }
+    {
+      temp = 70;
+      fan = 40;
+    }
+    {
+      temp = 75;
+      fan = 50;
+    }
+    {
+      temp = 80;
+      fan = 85;
+    }
+    {
+      temp = 90;
+      fan = 100;
+    }
   ];
 }
