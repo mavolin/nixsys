76464c2285cb3024bdb845ca4fab697710514172
mavolin <github@mavolin.co>
2023-12-31 17:16:01 +0100

    misc

---
+++ system/hardware-acceleration.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  boot.initrd.kernelModules = ["i915"];
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "iHD";
+    VDPAU_DRIVER = "va_gl";
+  };
+}
---
+++ system/services/printing.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  services.printing = {
+    enable = true;
+
+    drivers = with pkgs; [ epson-escpr ];
+
+    # The printers at Uni Hamburg on the compsci campus
+    browsedConf = ''
+      BrowsePoll linuxprint.informatik.uni-hamburg.de:631
+    '';
+  };
+}
