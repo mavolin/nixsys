ad10fa280bd9cbfaf2ca332c21728a122f9e5b01
mavolin <github@mavolin.co>
2025-04-13 21:14:30 +0200

    chore: remove borgmatic host from ssh config

--- user/programs/ssh.nix
+++ user/programs/ssh.nix
@@ -2,15 +2,6 @@
   programs.ssh = {
     enable = true;
     extraConfig = ''
-      Host backup
-        Hostname ${base.backup.host}
-        Port ${toString base.backup.port}
-        User borg
-        IdentityFile ${base.backup.identityFile}
-        IdentityAgent /
-        ServerAliveInterval 60
-        ServerAliveCountMax 240
-
       Host *
        IdentityAgent ~/.1password/agent.sock
     '';
