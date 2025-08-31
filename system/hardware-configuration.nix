133920822671d1a62de876c54ae4ca5dab66e231
mavolin <github@mavolin.co>
2025-08-31 16:58:53 +0200

    conf: add swapfile

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -39,7 +39,12 @@
     fsType = "vfat";
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 24 * 1024; # 24 GiB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
