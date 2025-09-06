ca8eed87ee91f5ef58fdb9825bf6b66c0b59d970
mavolin <github@mavolin.co>
2025-09-06 17:08:28 +0200

    conf: move tmp out of ram again

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -41,12 +41,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 24 * 1024; # 24 GiB
-    }
-  ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
